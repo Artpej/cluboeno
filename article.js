30170208cbd976346439f8fc93916308f3c0facd
Romain CHAPON <romainchapon@imac-de-romain.home>
2018-10-01 23:41:54 +0200

    minor correction

--- article.js
+++ article.js
@@ -2,7 +2,7 @@
 var uri = document.location.href;
 var components = URI.parse(uri);
 var query = URI.parseQuery(components['query']);
-var idwriter = 1;
+var idwriter = null;
 
 
 /**************** C O M P O N E N T   A C T I V A T I O N ****************/
